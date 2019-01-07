518bd0bf4a075d91043505c205a890d69d8b4d71
Eqram <eqram2011@gmail.com>
2019-01-07 20:41:14 +0600

    todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 
 class App extends Component {
+
+  state={
+    inputArray:[],
+    todoText:'',
+    count:0
+  }
+
+  doClick=()=>{
+    let tempArray = this.state.inputArray;
+    let tempCount = this.state.count;
+    tempArray.push({
+      id:tempCount++,
+      taskTest:this.state.todoText
+    })
+    this.setState({
+      inputArray:tempArray, count:tempCount
+    })
+  }
+
+  deleteClick=(e)=>{
+    var array = this.state.inputArray;
+    let tempCount = this.state.count;
+    var index = array.findIndex(x => x.id===e);
+
+    //console.log(index);
+    array.splice(index,1);;
+    this.setState({inputArray:array, count:tempCount});
+    //console.log(e);
+  }
+
+  handleChange = (e) => {
+    if (e.target.name === "taskTest")
+      this.setState({ todoText: e.target.value });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Eqram Learn React!
-          </a>
-        </header>
+
+      <div>
+          <TextField name='taskTest' value={this.state.todoText} placeholder='enter task' onChange={this.handleChange} />
+          <Button onClick={this.doClick} > ADD </Button>
+          <div>
+          {this.state.inputArray.map(item => 
+          <div key={item.id}>
+            <TextField name='id' value={item.id}/>
+            <TextField name='taskTest' value={item.taskTest}/>
+            <Button name='btnDel' onClick={()=>{ this.deleteClick(item.id) }} > Delete </Button>
+            </div>)}
+        </div>
+      
+
+
       </div>
     );
   }
