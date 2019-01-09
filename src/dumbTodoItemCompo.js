0e06aceca5a196ac6b9168d17785901bf8032ed2
Eqram <eqram2011@gmail.com>
2019-01-09 18:21:47 +0600

    Update dumbTodoItemCompo.js
    const { id, name, doDelete } = props;

--- src/dumbTodoItemCompo.js
+++ src/dumbTodoItemCompo.js
@@ -6,11 +6,13 @@ import PropTypes from 'prop-types';
 
 
 function DumbTodoItem(props) {
+    const { id, name, doDelete } = props;
+    
     return (
         <Paper style={{ width: '15%' }}>
-            <span> {props.id} </span>
-            <TextField name='taskTest' value={props.name} />
-            <Button name='btnDel' onClick={props.doDelete}> Delete </Button>
+            <span> {id} </span>
+            <TextField name='taskTest' value={name} />
+            <Button name='btnDel' onClick={doDelete}> Delete </Button>
         </Paper>
     );
 }
